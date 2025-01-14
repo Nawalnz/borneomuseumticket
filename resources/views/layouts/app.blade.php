d6debc180226e784ddb7fbdc30e699dd087c4338
Nawal Zarzaie <md.nawalnz@gmail.com>
2025-01-14 23:03:55 +0800

    add layout.app & edit guest layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,16 +22,16 @@
         @include('partials.navbar')
 
         <!-- Main Content -->
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
+        <div class="bg-gray-100 dark:bg-gray-900 min-h-screen">
+            <header class="bg-white dark:bg-gray-800 shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    @yield('header')
+                </div>
+            </header>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
-            </div>
+            <main class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                @yield('content')
+            </main>
         </div>
     </body>
 </html>
