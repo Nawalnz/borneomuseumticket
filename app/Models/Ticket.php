89e8506e0b731cf964c85e2d878794b5da1b51e7
Nawal Zarzaie <md.nawalnz@gmail.com>
2025-01-21 18:20:19 +0800

    added TicketBooking model, modify Ticket model

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -10,10 +10,30 @@ class Ticket extends Model
     use HasFactory;
 
     protected $fillable = [
-        'name',
-        'email',
-        'date',
-        'time',
-        'category_id', // Foreign key for the category
+        'booking_id', // Links to ticket_bookings
+        'name',       // Name of the user
+        'email',      // Email of the user
+        'date',       // Date of the ticket
+        'category_id',// Foreign key for the category
+        'is_used',    // Whether the ticket has been used
+        'is_expired', // Whether the ticket has expired
     ];
+
+    /**
+     * Relationship with TicketBooking.
+     * Each ticket belongs to one ticket booking.
+     */
+    public function booking()
+    {
+        return $this->belongsTo(TicketBooking::class, 'booking_id', 'id');
+    }
+
+    /**
+     * Relationship with Category.
+     * Each ticket belongs to one category.
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
 }
